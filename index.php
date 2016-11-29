142c29dff77eb67abd35df2ff6de21b09439dd05
adeleuze <antoine.deleuze@isep.fr>
2016-11-29 12:36:37 +0100

    changement de fichiers

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+    session_start();
+    require("Modele/connexion.php");
+    require("Vues/accueil.php");
+   
+    if(!isset($_SESSION["userID"])){ // L'utilisateur n'est pas connecté
+        include("Controleur/connexion.php"); // On utilise un controleur secondaire pour éviter d'avoir un fichier index.php trop gros
+    } else { // L'utilisateur est connecté
+        if(isset($_GET['cible'])) { // on regarde la page où il veut aller
+            if($_GET['cible'] == 'accueil'){
+                include("Vues/etat.php");
+            } else if ($_GET['cible'] == "etape1"){
+                include("Modele/utilisateurs.php");
+                $reponse = utilisateurs($db);
+                include("Vues/contact.php");
+           } else if ($_GET['cible'] == "deconnexion"){
+                // Détruit toutes les variables de session
+                $_SESSION = array();
+                if (isset($_COOKIE[session_name()])) {
+                    setcookie(session_name(), '', time()-42000, '/');
+                }
+                session_destroy();
+                include("Vues/accueil.php");
+            }
+        } else { // affichage par défaut
+                include("Vues/etat.php");
+        }
+    }
