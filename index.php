226d1bd4124b964609a9142ee4c78690cac61e1c
adeleuze <antoine.deleuze@isep.fr>
2016-12-02 21:28:37 +0100

    conversion en php

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php 
     session_start();
     require("Modele/connexion.php");
-    require("Vues/accueil.html");
+    require("Vues/accueil.php");
    
     if(!isset($_SESSION["userID"])){ // L'utilisateur n'est pas connecté
         include("Controleur/connexion.php"); // On utilise un controleur secondaire pour éviter d'avoir un fichier index.php trop gros
     } else { // L'utilisateur est connecté
         if(isset($_GET['cible'])) { // on regarde la page où il veut aller
-            if($_GET['cible'] == 'accueil'){
+            if($_GET['cible'] == 'etat'){
                 include("Vues/etat.php");
-            } else if ($_GET['cible'] == "etape1"){
+            } else if ($_GET['cible'] == "moncompte"){
                 include("Modele/utilisateurs.php");
                 $reponse = utilisateurs($db);
-                include("Vues/contact.php");
-           } else if ($_GET['cible'] == "deconnexion"){
+                include("Vues/moncompte.php");
+            } else if ($_GET['cible'] == "contact"){
+                include("Vue/contact.php");    
+            } else if ($_GET['cible'] == "deconnexion"){
                 // Détruit toutes les variables de session
                 $_SESSION = array();
                 if (isset($_COOKIE[session_name()])) {
                     setcookie(session_name(), '', time()-42000, '/');
                 }
                 session_destroy();
-                include("Vues/accueil.html");
+                include("Vues/accueil.php");
             }
         } else { // affichage par défaut
-                include("Vues/etat.html");
+                include("Vues/etat.php");
         }
     }
 ?>
