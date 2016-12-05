75d0fe035546271ac32abd983eb637aff9753d94
adeleuze <antoine.deleuze@isep.fr>
2016-12-05 14:32:23 +0100

    conv de accueil en php

---
+++ Vues/accueil.php
@@ -0,0 +1,7 @@
+<?php
+    $menu = menu("accueil");
+    $contenu = "<h2>Contenu de l'accueil</h2>";
+    $pied = pied();
+
+    include 'gabarit.php';
+?>
--- Vues/gabarit.php
+++ Vues/gabarit.php
@@ -1,12 +1,12 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>HomeWeb</title>
 		<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="styles/main.css" />
-	</head>
-	<body>
-		<div id="page">
+		<link rel="stylesheet" type="text/css" href="Styles/main.css" />
+	   <title>HomeWeb</title>
+    </head>
+    
+    <div id="global">
 		<!-- Header -->
             <header>
                 <div id="logo">
@@ -14,13 +14,14 @@
                 </div>            
                 <nav>
                     <ul>
-                        <div id="actuel"><li><a href="Vues/accueil.php">Accueil</a></li></div>
+                        <li><a href="Vues/accueil.php">Accueil</a></li>
                         <li><a href="contact.php">Contact</a></li>
                         <li><a href="moncompte.php">Mon compte</a></li>
                         <li><a href="etat.php">Etat</a></li>
                     </ul>
                 </nav>
             </header>
+                
 		<!-- Body -->
 			<section class="loginform cf">
                 <div id=formlogin>
