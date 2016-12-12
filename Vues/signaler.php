4a38ab101e695f234f8fa2291de72dbdd781ea81
Noé <noefaure@orange.fr>
2016-12-12 14:50:46 +0100

    Page signaler un capteur

---
+++ Vues/signaler.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>HomeWeb</title>
+		<meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="Styles/main.css" />
+	</head>
+	<body>
+		<div id="page">
+		<!-- Header -->
+            <header> <!-- En tete -->
+                <div id="logo">
+                    <a href="image/logo.png"><img src="image/logomini.png" alt="Logo HomeWeb" />
+                </div>
+                <nav>
+                    <ul>
+                        <div><li><a href="Vues/accueil.php">Accueil</a></li></div>
+                        <li><a href="Vues/contact.php">Contact</a></li>
+                        <li><a href="Vues/moncompte.php">Mon compte</a></li>
+                        <li><a href="Vues/etat.php">Etat</a></li>
+                    </ul>
+                </nav>
+            </header>
+		<!-- Body -->
+			<section class="loginform cf"> <!--formulaire d'identification-->
+                <div id=formsignup>
+                    <form name="login" action="Vues/etat.php" method="get" accept-charset="utf-8">
+					   <h1> Signaler un problème </h1>
+                        <div id="signupinput">
+                            <p> <b>Référence du capteur défaillant</b><span class="champoblig"> * </span></p>
+                            
+                            <input type="text" name="capteurD" required placeholder="Ex : 123456789">
+                            
+                            <p><br><b>Décrivez la nature du problème</b><span class="champoblig"> * </span>
+                            </p>
+                            
+                            <input type="text" class="description" name="descriptioncapteurD" required placeholder="Ex : Le capteur ne répond plus">
+                            
+                            <input class="boutons_piece" type="submit" value="Signaler le capteur" onClick="window.location.href='signup.html'">
+                            
+                        </div>
+                    </form>
+                </div>
+			</section>
+		</div>
+	</body>
+</html>
