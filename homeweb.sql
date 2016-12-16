94dbdc3e79374d7682b133ae26e84e638029c3f2
Noé <noefaure@orange.fr>
2016-12-16 10:04:47 +0100

    SQL

--- homeweb.sql
+++ homeweb.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.5
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost:3306
--- Généré le :  Ven 09 Décembre 2016 à 09:10
--- Version du serveur :  5.5.49-log
--- Version de PHP :  7.0.9
+-- Client :  localhost:8889
+-- Généré le :  Ven 16 Décembre 2016 à 10:04
+-- Version du serveur :  5.6.33
+-- Version de PHP :  7.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `homeweb`
@@ -23,33 +17,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `client`
---
-
-CREATE TABLE IF NOT EXISTS `client` (
-  `Nom` text NOT NULL,
-  `Prénom` text NOT NULL,
-  `Identifiant` int(10) NOT NULL,
-  `Adresse postale` text NOT NULL,
-  `Adresse mail` varchar(40) NOT NULL,
-  `Mot de passe` varchar(10) NOT NULL,
-  `Numéro de téléphone` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `client`
---
-
-INSERT INTO `client` (`Nom`, `Prénom`, `Identifiant`, `Adresse postale`, `Adresse mail`, `Mot de passe`, `Numéro de téléphone`) VALUES
-('Breviere', 'Maxime', 123456789, 'spmahc sed eur', 'maxime@', 'maxime', '565656565656');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `maintenance`
 --
 
-CREATE TABLE IF NOT EXISTS `maintenance` (
+CREATE TABLE `maintenance` (
   `Nom` text NOT NULL,
   `Prénom` text NOT NULL,
   `Identifiant` int(10) NOT NULL,
@@ -59,10 +30,34 @@ CREATE TABLE IF NOT EXISTS `maintenance` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `Utilisateurs`
+--
+
+CREATE TABLE `Utilisateurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `adresse` text NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `anniversaire` date NOT NULL,
+  `tel` int(20) NOT NULL,
+  `mdp` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `Utilisateurs`
+--
+
+INSERT INTO `Utilisateurs` (`id`, `nom`, `prenom`, `adresse`, `mail`, `anniversaire`, `tel`, `mdp`) VALUES
+(1, 'a', 'a', 'a', 'a@a', '2016-12-08', 1, '1');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `webmaster`
 --
 
-CREATE TABLE IF NOT EXISTS `webmaster` (
+CREATE TABLE `webmaster` (
   `Nom` text NOT NULL,
   `Prénom` text NOT NULL,
   `Identifiant` int(10) NOT NULL,
@@ -74,18 +69,17 @@ CREATE TABLE IF NOT EXISTS `webmaster` (
 --
 
 --
--- Index pour la table `client`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`Identifiant`),
-  ADD UNIQUE KEY `Identifiant` (`Identifiant`);
-
---
 -- Index pour la table `maintenance`
 --
 ALTER TABLE `maintenance`
   ADD PRIMARY KEY (`Identifiant`),
   ADD UNIQUE KEY `Identifiant` (`Identifiant`);
+
+--
+-- Index pour la table `Utilisateurs`
+--
+ALTER TABLE `Utilisateurs`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `webmaster`
@@ -94,6 +88,12 @@ ALTER TABLE `webmaster`
   ADD PRIMARY KEY (`Identifiant`),
   ADD UNIQUE KEY `Identifiant` (`Identifiant`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Utilisateurs`
+--
+ALTER TABLE `Utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
