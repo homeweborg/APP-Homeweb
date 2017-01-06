b38e935d35771305fb3c4a25a4951152f2b829f3
mbreviere <maxime.breviere@isep.fr>
2017-01-06 11:51:38 +0100

    modifications signup + css

--- Vues/signup.php
+++ Vues/signup.php
@@ -20,29 +20,19 @@
 				    <form name="login" action="../Controleur/inscription.php" method="post" accept-charset="utf-8">
                         <h1> Bienvenue sur HomeWeb </h1>
                         <div id="signupinput">
-                            <p>Nom de famille <span class="champoblig">*</span></p>
-                            <input type="text" name="nom" required placeholder="Ex : Rimbaud">
-                            <p>Prénom <span class="champoblig">*</span></p>
-                            <input type="text" name="prenom" required placeholder="Ex : Arthur">
-                            <p>Adresse Postale <span class="champoblig">*</span></p>
-                            <input type="text" name="adresse" required placeholder="Ex : 12 rue Napoléon, 08105, Charleville">
-                            <p>Adresse mail <span class="champoblig">*</span></p>
-                            <input type="email" name="mail" required placeholder="Ex : arthur.rimbaud@exemple.fr">
-                            <p>Votre Anniversaire <span class="champoblig">*</span></p>
-                            <input type="date" name="anniversaire" required placeholder="10/11/1891">
-                            <p>Numéro de téléphone <span class="champoblig">*</span></p>
-                            <input type="tel" name="telephone" required placeholder="Ex : 0612345678">
-                            <p>Choisissez un mot de passe <span class="champoblig">*</span> </p>
+                            <p>Adresse mail :</p>
+                            <input type="mail" name="mail" required placeholder="Ex : domisep@isep.fr">
+                            <p>Choisissez un mot de passe :</p>
                             <input type="password" name="mdp" required placeholder="Ex : Motdepasse123">
-                            <p>Confirmer votre mot de passe <span class="champoblig">*</span></p>
+                            <p>Confirmer votre mot de passe :</p>
                             <input type="password" name="mdpc" required placeholder="Ex : Motdepasse123">
-                            <p>Numéro d'identification Homeweb <span class="champoblig">*</span></p>
-                            <input type="tel" name="Numero_Homeweb" required placeholder="Ex : 123456789">
+                            <p>Numéro de série d'un capteur :</p>
+                            <input type="string" name="Numero_Homeweb" required placeholder="Ex : A7TB0Y6TE86F">
                         </div>
                         <input id="boutons_signup" type="submit" name="bouton_submit" value="SIGN UP"/>
 				    </form>
+                <h2 class="infos_confidentialite">Tous les champs sont obligatoires.</h2>
                 </div>
-                <h2 class="infos_confidentialite">Les champs marqués d'une étoile <span class="champoblig">*</span>orange sont obligatoires, ces informations sont indispenssables au bon fonctionnement de la plateforme HomeWeb.<br><br>Ces informations sont personnelles, elles ne seront en aucun cas divulgées publiquement. Merci de votre confiance.<br><br>Les comptes Homeweb sont uniquement réservés au clients de notre plateforme. Le numéro d'identification vous a été fournie par Homeweb et permet d'attester que vous êtes vraiment client.</h2>
 			</section>
 		</div>
 		<!-- Footer -->
