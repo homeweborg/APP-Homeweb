f4b17d0801e8abc092251afb02cbc7937799149d
mbreviere <maxime.breviere@isep.fr>
2017-01-09 15:44:48 +0100

    footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php echo' <footer>
+                <nav>
+                    <ul>
+                        <li>Etat du service</li>
+                        <li>|</li>
+                        <li>Conditions</li>
+                        <li>|</li>
+                        <li>Confidentialité</li>
+                        <li>|</li>
+                        <li>Mentions légales</li>
+                    </ul>
+                </nav>
+            </footer>';?>
