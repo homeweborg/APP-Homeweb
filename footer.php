3d4ba1306c0d2cfe323462b1b561fda4ecb2ec86
mbreviere <maxime.breviere@isep.fr>
2017-01-16 17:01:32 +0100

    réseaux sociaux WIP

--- footer.php
+++ footer.php
@@ -6,6 +6,13 @@
                         <li><a href="#">Mentions légales</a></li>
                         <li>|</li>
                         <li><a href="#">Confidentialité</a></li>                        
+                    <div id=social>
+                        <ul>
+                        <li><a href="image/facebook.png"><img src="image/facebook.png" alt="Facebook" title="Facebook"/></li>
+                        <li><a href="image/twitter.png"><img src="image/twitter.png" alt="Twitter" title="Twitter"/></li>
+                        <li><a href="image/linkedin.png"><img src="image/linkedin.png" alt="Linkedin" title="Linkedin"/></li>
+                        </ul>
+                    </div>
                     </ul>
                 </nav>
             </footer>';?>
