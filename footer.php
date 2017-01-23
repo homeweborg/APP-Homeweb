e758462f36c6cd62ba530190ca6745b441bb3c0a
mbreviere <maxime.breviere@isep.fr>
2017-01-23 16:47:22 +0100

    zjeujfb

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<?php echo' <footer>
+<?php echo(' <footer>
                 <nav>
                     <ul>
                         <li><a href="#">Conditions</a></li>
@@ -8,11 +8,12 @@
                         <li><a href="#">Confidentialité</a></li>                        
                     <div id=social>
                         <ul>
-                        <li><a href="../image/facebook.png"><img src="image/facebook.png" alt="Facebook" title="Facebook"/></li>
-                        <li><a href="../image/twitter.png"><img src="image/twitter.png" alt="Twitter" title="Twitter"/></li>
-                        <li><a href="../image/linkedin.png"><img src="image/linkedin.png" alt="Linkedin" title="Linkedin"/></li>
+                        <li><a href="image/facebook.png"><img src="image/facebook.png" alt="Facebook" title="Facebook"/></li>
+                        <li><a href="image/twitter.png"><img src="image/twitter.png" alt="Twitter" title="Twitter"/></li>
+                        <li><a href="image/linkedin.png"><img src="image/linkedin.png" alt="Linkedin" title="Linkedin"/></li>
                         </ul>
                     </div>
                     </ul>
                 </nav>
-            </footer>';?>
+            </footer>');
+?>
