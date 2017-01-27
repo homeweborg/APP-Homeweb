b36218b2418591404f46e362d2aa84a0cb2372bd
Noé <noefaure@orange.fr>
2017-01-27 12:07:05 +0100

    bdd soigné

--- app-homeweb.sql
+++ app-homeweb.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `administrateur` (
   `mail` varchar(255) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   `numero_admin` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `administrateur`
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `capteur` (
   `etat` int(10) NOT NULL,
   `type` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `id_client` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `capteur`
@@ -88,7 +88,7 @@ CREATE TABLE IF NOT EXISTS `domisep_infos` (
   `id` int(11) NOT NULL,
   `nom` text COLLATE utf8_unicode_ci NOT NULL,
   `contenu` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) NGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `domisep_infos`
@@ -110,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `domisep_messagerie` (
   `mail` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `objet` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `demande` longtext COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `domisep_messagerie`
@@ -131,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `domisep_probleme` (
   `capteur` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
   `probleme` longtext COLLATE utf8_unicode_ci NOT NULL,
   `id_user` varchar(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `domisep_probleme`
@@ -152,7 +152,7 @@ CREATE TABLE IF NOT EXISTS `eau` (
   `etat` int(11) NOT NULL,
   `consomation` int(11) NOT NULL,
   `prix_eau` float NOT NULL DEFAULT '3.1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `eau`
@@ -172,7 +172,7 @@ CREATE TABLE IF NOT EXISTS `elec` (
   `etat` int(11) NOT NULL,
   `consomation` int(11) NOT NULL,
   `prix_elec` float NOT NULL DEFAULT '0.5691'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `elec`
@@ -192,7 +192,7 @@ CREATE TABLE IF NOT EXISTS `gaz` (
   `etat` int(11) NOT NULL,
   `consomation` int(11) NOT NULL,
   `prix_gaz` float NOT NULL DEFAULT '9.5'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `gaz`
@@ -221,7 +221,7 @@ CREATE TABLE IF NOT EXISTS `pieces` (
   `control_tech_l` date NOT NULL,
   `numero_capteur_l` varchar(250) NOT NULL,
   `consigne_temp` int(11) NOT NULL DEFAULT '25'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `pieces`
@@ -243,7 +243,7 @@ INSERT INTO `pieces` (`id`, `id_Utilisateur`, `Nom`, `presence_temp`, `presence_
 CREATE TABLE IF NOT EXISTS `porte` (
   `id` int(11) NOT NULL,
   `etat` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `porte`
@@ -266,7 +266,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `prenom` varchar(200) NOT NULL,
   `adresse` varchar(200) NOT NULL,
   `tel` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `utilisateurs`
@@ -283,6 +283,57 @@ INSERT INTO `utilisateurs` (`id`, `mail`, `mdp`, `Nom`, `prenom`, `adresse`, `te
 --
 -- Indexes for table `administrateur`
 --
+
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `domisep_infos`
+--
+ALTER TABLE `domisep_infos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `domisep_messagerie`
+--
+ALTER TABLE `domisep_messagerie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `domisep_probleme`
+--
+ALTER TABLE `domisep_probleme`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `eau`
+--
+ALTER TABLE `eau`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `elec`
+--
+ALTER TABLE `elec`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gaz`
+--
+ALTER TABLE `gaz`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `maintenance`
+--
+ALTER TABLE `maintenance`
+  ADD PRIMARY KEY (`Identifiant`),
+  ADD UNIQUE KEY `Identifiant` (`Identifiant`);
+
+--
+
 ALTER TABLE `administrateur`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`);
@@ -299,6 +350,83 @@ ALTER TABLE `capteur`
 ALTER TABLE `pieces`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `porte`
+--
+ALTER TABLE `porte`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrateur`
+--
+ALTER TABLE `administrateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `capteur`
+--
+ALTER TABLE `capteur`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `data`
+--
+ALTER TABLE `data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `domisep_infos`
+--
+ALTER TABLE `domisep_infos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `domisep_messagerie`
+--
+ALTER TABLE `domisep_messagerie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `domisep_probleme`
+--
+ALTER TABLE `domisep_probleme`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `eau`
+--
+ALTER TABLE `eau`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `elec`
+--
+ALTER TABLE `elec`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `gaz`
+--
+ALTER TABLE `gaz`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `pieces`
+--
+ALTER TABLE `pieces`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `porte`
+--
+ALTER TABLE `porte`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
